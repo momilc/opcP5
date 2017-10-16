69854bbaf6dd76cb02bdcd50aa6bcc4970745096
momilc <m.sakho@momilc.com>
2017-10-16 02:45:03 +0200

    Deleted folder config and config.php file inside that folder Changed config.php path name

--- app/App.php
+++ app/App.php
@@ -31,7 +31,7 @@ class  App {
     }
 
     public function getDb() {
-        $config = Config::getInstance(ROOT. '/config/config.php');
+        $config = Config::getInstance(ROOT. '/app/Config.php');
          if(is_null($this->db_instance)) {
              $this->db_instance = new MysqlDatabase($config->get('db_name'), $config->get('db_user'), $config->get('db_pass'), $config->get('db_host'));
          }
